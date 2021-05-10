b0d61a0a00f1cbc3b71ecffe2b5dca0f4495ca30
nereamonas <nmonasterio003@ikasle.ehu.eus>
2021-05-11 00:01:23 +0200

    correccion ranking

--- ejercicioExtraFinal/public/js/ranking.js
+++ ejercicioExtraFinal/public/js/ranking.js
@@ -1,22 +1,28 @@
+//Creamos un map para guardar los puntos / el nombre de la persona
+var mapRanking=new Map();
 
-var ranking=[]
 function getRanking() {
     // Cogeremos el localStorage y lo guardamos en un array para poder ordenar automaticamente
     rText=document.getElementById('ranking');
     for (var i=0; i < localStorage.length; i++) { //Recorremos todos los datos del localStorage
-
         var key=localStorage.key(i);  //Cogemos el key del elemento numero i
         if (key.includes("tetris")){  //Comprobamos si dentro del key esta la palabra postit para ver si pertenece a alguna que nos interese
             var puntuacion = localStorage.getItem(key); //Cogemos el texto del postit actual
-            ranking.push(puntuacion);
+            var split=puntuacion.split(' puntos - ');
+            mapRanking.set(' puntos - '+split[1],parseInt(split[0]));
         }
     }
-    ranking=ranking.sort();  //Ordenamos
-    ranking=ranking.reverse(); //Revertimos el orden porque lo guarda de manera ascendente
+
+    //Tendremos que ordenar el array por la cantidad de puntos
+    mapRanking[Symbol.iterator] = function* () {
+        yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
+    }
     var i=0;
-    for (i=0;i<ranking.length;i++){ //Recorremos los elementos y guardaremos los 10 primeros en el texto que se escribira en pantalla
-        if(i<10){
-            rText.innerText=rText.innerText+(i+1)+"º : "+ranking[i]+"\n";
+    //Recorremos los elementos para imprimirlos en la pantalla
+    for (let [key, value] of mapRanking) {
+        if (i < 10) {
+            i++;
+            rText.innerText = rText.innerText + (i) + "º : " + value + key + "\n";
         }
     }
 
